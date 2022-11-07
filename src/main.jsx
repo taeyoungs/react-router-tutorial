f441337be21654376ca29054bc4aae62618e4d44
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-07 17:01:21 +0900

    feat: Adding a Router
    src/main.jsx
    - enrty point인 main.jsx에 라우터의 중추가 될 root route 추가
    - BrowserRouter 사용
    - root route 하위에 경로가 있는 nested layout을 구성할 예정

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello World!</div>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
