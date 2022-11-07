8d43a704dfe784dcc94710dbbe6febf3839f841d
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-07 17:14:35 +0900

    feat: The Root Route
    routes/root.jsx
    - root route에 렌더링될 Root 컴포넌트 구현

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
+import Root from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello World!</div>,
+    element: <Root />,
   },
 ]);
 
