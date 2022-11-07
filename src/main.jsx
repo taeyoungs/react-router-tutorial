6576e5f2dc5381b416fe4311c444c5814a6efbd0
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-07 17:38:53 +0900

    feat: The Contact Route UI
    routes/contact.jsx
    - 상세 페이지를 컨텐츠를 담당할 Contact 컴포넌트 구현

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import Root from './routes/root';
+import Contact from './routes/contact';
 import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact />,
   },
 ]);
 
