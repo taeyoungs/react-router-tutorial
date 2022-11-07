de707ac5e678800c19d8e03b8d367577edfa8580
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-07 17:41:36 +0900

    mod: Nested Routes
    routes/root.jsx
    - Contact 컴포넌트를 Root 컴포넌트 내 div[id=detail]에 위치시키기
      위해 독립된 경로가 아닌 Nested Routes 형태로 변경
    - children 속성에 경로와 컴포넌트를 위치시키면 추후에 해당 경로와
      일치하는 경우 상위 컴포컴포넌트에서 Outlet 컴포넌트를 이용하여
      경로에 담긴 컴포넌트를 렌더링할 수 있다.
    
    src/main.jsx
    - children 속성에 내려준 경로에 일치하는 경우 내려준 컴포넌트를
      렌더링하기 위해 Outlet 컴포넌트를 추가
    - children 속성에 넣어준 경로와 일치할 경우 해당 경로와 연결된
      element 컴포넌트가 Outlet 컴포넌트 자리에 렌더링된다.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 export default function Root() {
   return (
     <>
@@ -24,7 +26,9 @@ export default function Root() {
           </ul>
         </nav>
       </div>
-      <div id="detail"></div>
+      <div id="detail">
+        <Outlet />
+      </div>
     </>
   );
 }
