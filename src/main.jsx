81d776f63f9c4bbf66ca36d75632597e3317a30f
Taeyoung Jang <xoxodudwkd@gmail.com>
2022-11-08 21:06:35 +0900

    mod: Index Routes

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Root, { loader as rootLoader, action as rootAction } from './routes/root'
 import Contact, { loader as contactLoader } from './routes/contact';
 import EditContact, { action as editAction } from './routes/edit';
 import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
 import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
@@ -16,6 +17,10 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      {
+        index: true,
+        element: <Index />,
+      },
       {
         path: 'contacts/:contactId',
         element: <Contact />,
